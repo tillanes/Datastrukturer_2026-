7d228cf2c78083fdaa4bc6cadf4214fb3f4957fb
tillanes <tillanes@arcada.fi>
2026-02-11 12:00:18 +0200

    Linked list

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,25 @@ public class Main {
     public static void main(String[] args) {
         String [] testData = {"Walter","Jesse","Skyler","Hank","Marie","Saul","Gus","Mike","Walter","Lydia","Todd","Tuco","Hector","Steve","Jane","Skinny","Badger","Andrea","Brock","Gale","Ted","Tyrus","Victor","Combo","Emilio","Domingo","Eladio","Juan","Leonel","Marco","Carmen","Gretchen","Elliott","Bogdan","Huell","Patrick","Joe","Francesca","Ed","Declan"};
 
+        Person p1 = new Person(testData[0]);
+        Person p2 = new Person(testData[1], p1);
+        Person p3 = new Person(testData[0], p2);
+        Person p4 = new Person(testData[1], p3);
+
+        Person pMid = new Person("Midperson", p2);
+        p3.next = pMid;
+
+
+
+        Person current = p4;
+        while(current != null){
+            System.out.println(current.getName());
+            current = current.next;
+        }
+
+
+        System.exit(0);
         StringArray names = new StringArray(testData);
-
-
-
-
 
         System.out.println(names.toString());
         names.insert(1,"Marge");
